a887ef9b1b6554d068d1adcef67c1a9f2004b5e4
juaalvarado <juan110470@gmail.com>
2020-01-02 10:53:33 -0800

    React JS files

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ import Count    from './components/Count'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
     constructor(props) {
@@ -18,23 +19,29 @@ class App extends React.Component {
         this.state = {
             theme: 'light',
             toggleTheme: () => {
-                this.setState(( { theme }) => ({
+                this.setState(({theme}) => ({
                     theme: theme === 'light' ? 'dark' : 'light'
                 }))
-            }
+            },
+            checked: 'false'
         }
     }
 
+
     render() {
         return(
-            <ThemeProvider value={this.state}>
-                <div className={this.state.theme}>
-                    <div className='container'>
-                        <Nav />
-                        <Battle />
+            <Router>
+                <ThemeProvider value={this.state}>
+                    <div className={this.state.theme}>
+                        <div className='container'>
+                            <Nav />
+                            <Route exact path='/' component={Popular} />
+                            <Route exact path='/battle' component={Battle} />
+                            <Battle />
+                        </div>
                     </div>
-                </div>
-            </ThemeProvider>
+                </ThemeProvider>
+            </Router>
         )
     }
 }
